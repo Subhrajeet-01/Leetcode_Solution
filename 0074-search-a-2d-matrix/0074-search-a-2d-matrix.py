4d78c13a4b0732185c3ed88b0505b063e90aa5ad
Subhrajeet-01 <115855197+Subhrajeet-01@users.noreply.github.com>
2023-08-07 14:16:45 +0530

    Time: 31 ms (47.29%), Space: 13.8 MB (40.45%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        for i in range(len(matrix)):
+            if target in matrix[i]:
+                return True
+        return False
