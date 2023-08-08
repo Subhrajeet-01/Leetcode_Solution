f46ba82648ae7f33d90a8843b1245e2ad48bf7fe
Subhrajeet-01 <115855197+Subhrajeet-01@users.noreply.github.com>
2023-08-08 18:31:52 +0530

    Time: 11 ms (96.40%), Space: 13.4 MB (48.07%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def lastStoneWeight(self, stones):
+        """
+        :type stones: List[int]
+        :rtype: int
+        """
+        while len(stones)>1:
+            
+            y = max(stones)
+            stones.remove(y)
+            x = max(stones)
+            stones.remove(x)
+            if x != y:
+                z = abs(x-y)
+                stones.append(z)
+        if stones:
+            return stones[0]
+        return 0
+        
