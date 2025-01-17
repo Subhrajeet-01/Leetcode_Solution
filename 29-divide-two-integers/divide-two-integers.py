26cc2290f5ff3870c4502e0f7bbddf5339995898
Subhrajeet Mahapatra <115855197+Subhrajeet-01@users.noreply.github.com>
2025-01-17 16:11:32 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (12.55%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.py
+++ 29-divide-two-integers/divide-two-integers.py
@@ -1,9 +1,30 @@
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
-        if dividend == -2147483648 and divisor == -1:
-            return 2147483647
+        if dividend == -2**31 and divisor == -1:
+            return 2**31 - 1
+        if dividend == -2**31 and divisor == 1:
+            return -2**31
+        if (dividend < 0) ^ (divisor < 0):
+            sign = -1 
         else:
-            division = dividend/divisor
-            if division > 0:
-                return floor(division)
-            return ceil(division)
+            sign = 1
+        a = abs(dividend)
+        b = abs(divisor)
+        quotient = 0
+        while a >= b:
+            tmp = b
+            m = 1
+            while (tmp << 1) <= a:
+                tmp <<= 1
+                m <<= 1
+            a -= tmp
+            quotient += m
+        return quotient * sign
+        
+        # if dividend == -2147483648 and divisor == -1:
+        #     return 2147483647
+        # else:
+        #     division = dividend/divisor
+        #     if division > 0:
+        #         return floor(division)
+        #     return ceil(division)
